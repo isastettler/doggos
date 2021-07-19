86cd32784d1f6180f9ce2a8046a61272f4b81355
Isabelle Stettler <aysa.stettler@gmail.com>
2021-07-19 17:24:07 -0400

    add serach bar

--- src/BreedList.js
+++ src/BreedList.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
 import BreedItem from "./BreedItem";
+import './index.css'
 
 const breedsListApi = "https://dog.ceo/api/breeds/list/all";
 
 function BreedList() {
 	const [allBreeds, setAllBreeds] = useState([]);
+	const [breedSearch, setBreedSearch] = useState('');
 	//fetch the breeds list from dog api
 	const getBreedsList = async () => {
 		const response = await fetch(breedsListApi);
 		const { message } = await response.json();
+		console.log(message)
 		const breedsList = Object.keys(message);
 		setAllBreeds(breedsList);
 	};
@@ -19,7 +22,8 @@ function BreedList() {
 	return (
 		<div className="container">
 			<h2>Dog List goes here </h2>
-			{allBreeds.map((breed, idx) => (
+			<input placeholder="search fav breed..." onChange={(e) => setBreedSearch(e.target.value)}/>
+			{allBreeds.filter(breed => breed.slice(0, breedSearch.length) === breedSearch).map((breed, idx) => (
 				<BreedItem key={idx} breed={breed} />
 			))}
 		</div>
